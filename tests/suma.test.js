b998d9ee420b3107e7112b00019f7aec36823131
keven <k.e.v.i.n.l.o.p.e.z1232007@gmail.com>
2025-12-02 11:18:17 -0500

    fix: resolve node test import error by using .cjs extension

--- tests/suma.test.js
+++
@@ -1,6 +0,0 @@
-// Importa la función de suma (Asegúrate de que la ruta sea correcta)
-const { strictEqual } = require('assert');
-const { suma } = require('../src/app');
-
-strictEqual(suma(1, 2), 3, 'La suma de 1 + 2 debe ser 3');
-strictEqual(suma(-1, 1), 0, 'La suma de -1 + 1 debe ser 0');
